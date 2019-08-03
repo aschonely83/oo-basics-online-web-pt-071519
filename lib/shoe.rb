00685bd16539cc4ec2d1d7a1cee9a14ceb0c2b96
Adam Schonely <aschonely@gmail.com>
2019-08-03 21:18:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition 
-  
+  attr_accessor :brand, :color, :size, :material 
+  attr_reader :condition
   def initialize(brand)
     @brand = brand
   end
